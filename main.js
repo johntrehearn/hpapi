c4f9d7ec996ebfdb7a1a3a0f5cb1894b4b2fcb43
John Trehearn <johntrehearnbc@gmail.com>
2024-09-11 16:20:04 +0300

    buttons and search working

--- main.js
+++ main.js
@@ -3,34 +3,40 @@ const groupButtons = document.querySelectorAll('.btn');
 const searchBar = document.getElementById('searchbar');
 
 let potterData = [];
-
-let groupData = 'characters';
+let groupData = '';
 
 const fetchData = async () => {
-    await fetch(`http://hp-api.herokuapp.com/api/${groupData}`)
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data); 
+    let urlfetch = groupData === 'students' || groupData === 'staff' 
+        ? `http://hp-api.herokuapp.com/api/characters/${groupData}` 
+        : `https://hp-api.onrender.com/api/spells`;
 
-        const fetches = data.map((item) => {
-            return {
-                name: item.name,
-                img: item.image,
-                anscestry: item.ancestry,
-                house: item.house,
-                wand: {
-                    wood: item.wand.wood,
-                    core: item.wand.core,
-                    length: item.wand.length
-                },
-                patronus: item.patronus,
-            };
-    });
-      
-    potterData = fetches;
-    potterCards('');
+    await fetch(urlfetch)
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
 
-    });
+            if (groupData === 'students' || groupData === 'staff') {
+                potterData = data.map((item) => ({
+                    name: item.name,
+                    img: item.image,
+                    ancestry: item.ancestry,
+                    house: item.house,
+                    wand: {
+                        wood: item.wand.wood,
+                        core: item.wand.core,
+                        length: item.wand.length
+                    },
+                    patronus: item.patronus,
+                }));
+            } else {
+                potterData = data.map((item) => ({
+                    name: item.name,
+                    description: item.description
+                }));
+            }
+
+            potterCards('');
+        });
 };
 
 const toSentenceCase = (str) => {
@@ -40,24 +46,29 @@ const toSentenceCase = (str) => {
 
 const potterCards = (searchString) => {
     const cards = potterData
-    .filter((data) => {
-        return data.name.toLowerCase().includes(searchString);
-    })
-    .map((data) => {
-        return `<div class="card">
-          <div>
-            <img src="${data.img}" alt="${data.name}"/>
-           </div>
-            <div>
-        <p class="description"> Name: ${data.name ? data.name.toUpperCase() : 'None'} </p>
-        <p class="description"> Ancestry: ${data.anscestry ? toSentenceCase(data.anscestry) : 'None'} </p>
-        <p class="description"> House: ${data.house ? toSentenceCase(data.house) : 'None'} </p>
-        <p class="description"> Wand: ${data.wand.wood ? toSentenceCase(data.wand.wood) : 'None'} ${data.wand.core ? toSentenceCase(data.wand.core) : ''} ${data.wand.length ? data.wand.length + ' inches' : ''} </p>
-        <p class="description"> Patronus: ${data.patronus ? toSentenceCase(data.patronus) : 'None'} </p>
-        </div>
-        </div>`;
-    })
-    .join('');
+        .filter((data) => data.name.toLowerCase().includes(searchString))
+        .map((data) => {
+            if (groupData === 'students' || groupData === 'staff') {
+                return `<div class="card">
+                    <div>
+                        <img src="${data.img}" alt="${data.name}"/>
+                    </div>
+                    <div>
+                        <p class="description"> Name: ${data.name ? data.name.toUpperCase() : 'None'} </p>
+                        <p class="description"> Ancestry: ${data.ancestry ? toSentenceCase(data.ancestry) : 'None'} </p>
+                        <p class="description"> House: ${data.house ? toSentenceCase(data.house) : 'None'} </p>
+                        <p class="description"> Wand: ${data.wand.wood ? toSentenceCase(data.wand.wood) : 'None'} ${data.wand.core ? toSentenceCase(data.wand.core) : ''} ${data.wand.length ? data.wand.length + ' inches' : ''} </p>
+                        <p class="description"> Patronus: ${data.patronus ? toSentenceCase(data.patronus) : 'None'} </p>
+                    </div>
+                </div>`;
+            } else {
+                return `<div class="card">
+                    <p class="description"> Spell: ${data.name ? data.name.toUpperCase() : 'None'} </p>
+                    <p class="description"> Description: ${data.description ? toSentenceCase(data.description) : 'None'} </p>
+                </div>`;
+            }
+        })
+        .join('');
     potterDisplay.innerHTML = cards;
 };
 
@@ -67,4 +78,15 @@ searchBar.addEventListener('keyup', (e) => {
     potterCards(searchString);
 });
 
+const groupBut = () => {
+    groupButtons.forEach((button) => 
+        button.addEventListener('click', () => {
+            groupData = button.getAttribute('id');
+            console.log('Button INFO is ', groupData);
+            fetchData(); 
+        })
+    );
+};
+
 fetchData();
+groupBut();
